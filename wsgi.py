934773cc454b9cde005ca9ec18093e0a9ae676ab
Aisha Huseynzada <97541335+hsynzd@users.noreply.github.com>
2024-08-02 22:43:12 +0400

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,7 @@
-from main import app  # Adjust 'main' to the actual module name where your Flask 'app' is defined
+from main import app
 
 if __name__ == "__main__":
     from waitress import serve
-    serve(app, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+    import os
+    port = int(os.environ.get('PORT', 8080))
+    serve(app, host='0.0.0.0', port=port)
